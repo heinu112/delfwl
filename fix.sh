500101cf560809be0d0f9ee307971fc275a77be6
heinu112 <81513657+heinu112@users.noreply.github.com>
2022-05-27 10:04:08 +0800

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,51 +1,32 @@
 #!/bin/bash
-if [ -f /etc/redhat-release ]; then
+if [[ -f /etc/redhat-release ]]; then
     systemcommand="yum"
 elif cat /etc/issue | grep -Eqi "debian"; then
-    release="debian"
-    apt-get install tar
+    systemcommand="apt"
 elif cat /etc/issue | grep -Eqi "ubuntu"; then
     systemcommand="apt"
 elif cat /etc/issue | grep -Eqi "centos|red hat|redhat|rocky|alma|oracle linux"; then
     systemcommand="yum"
 elif cat /proc/version | grep -Eqi "debian"; then
-    release="debian"
-    apt-get install tar
+    systemcommand="apt"
 elif cat /proc/version | grep -Eqi "ubuntu"; then
     systemcommand="apt"
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat|rocky|alma|oracle linux"; then
     systemcommand="yum"
 else
-    echo -e "错误: 未检测到系统版本\n"
-    exit 1
+    echo -e "错误: 未检测到系统版本\n" && exit 1
 fi
-if [ -f /etc/os-release ]; then
-    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
-fi
-if [ -z "$os_version" && -f /etc/lsb-release ]; then
-    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
-fi
-
-if [ x"${release}" == x"centos" ]; then
-    if [[ ${os_version} -le 6 ]]; then
-        echo -e "错误: 请使用 CentOS 7 或更高版本的系统！\n"
-        exit 1
-    fi
-elif [ x"${release}" == x"ubuntu" ]; then
-    if [ ${os_version} -lt 16 ]; then
-        echo -e "错误: 请使用 Ubuntu 16 或更高版本的系统！\n"
-        exit 1
-    fi
-elif [ x"${release}" == x"debian" ]; then
-    if [ ${os_version} -lt 8 ]; then
-        echo -e "错误: 请使用 Debian 8 或更高版本的系统！\n"
-        :exit 1
-    fi
-fi
+if [ $systemcommand ];then
   if [ `id -u` != 0 ];then
     echo -e "错误: 仅限 root 用户执行"
   fi  
   if [ $systemcommand == "yum" ]; then
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -F
+    iptables-save
+    iptables-persistent
     systemctl stop firewalld.service
     systemctl disable firewalld.service
     systemctl stop iptables
@@ -57,10 +38,15 @@ fi
     yum update -y
 	yum install -y iproute
   elif  [ $systemcommand == "apt" ]; then
-      iptables -F
-    iptables -t nat -F
-    iptables -P ACCEPT
-    iptables -t nat -P ACCEPT
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -F
+    iptables-save
+    iptables-persistent
+    apt-get install iptables-persistent
+    netfilter-persistent save
+    netfilter-persistent reload
     service stop iptables
     apt-get remove -y iptables
     ufw disable
@@ -68,4 +54,8 @@ fi
     apt-get update -y
 	apt-get install -y iproute2
   fi
-  echo -e "完成"
+    echo -e "完成"
+   else
+    echo -e "错误: 未检测到系统版本\n"
+    exit 1
+    fi
