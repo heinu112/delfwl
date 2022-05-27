b14b451888faf50b1d54b6c9d1e8f7d849a0e437
heinu112 <81513657+heinu112@users.noreply.github.com>
2022-05-27 09:54:44 +0800

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ -f /etc/redhat-release ]]; then
+if [ -f /etc/redhat-release ]; then
     systemcommand="yum"
 elif cat /etc/issue | grep -Eqi "debian"; then
     release="debian"
@@ -19,25 +19,25 @@ else
     echo -e "错误: 未检测到系统版本\n"
     exit 1
 fi
-if [[ -f /etc/os-release ]]; then
+if [ -f /etc/os-release ]; then
     os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
 fi
-if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+if [ -z "$os_version" && -f /etc/lsb-release ]; then
     os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
 fi
 
-if [[ x"${release}" == x"centos" ]]; then
+if [ x"${release}" == x"centos" ]; then
     if [[ ${os_version} -le 6 ]]; then
         echo -e "错误: 请使用 CentOS 7 或更高版本的系统！\n"
         exit 1
     fi
-elif [[ x"${release}" == x"ubuntu" ]]; then
-    if [[ ${os_version} -lt 16 ]]; then
+elif [ x"${release}" == x"ubuntu" ]; then
+    if [ ${os_version} -lt 16 ]; then
         echo -e "错误: 请使用 Ubuntu 16 或更高版本的系统！\n"
         exit 1
     fi
-elif [[ x"${release}" == x"debian" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
+elif [ x"${release}" == x"debian" ]; then
+    if [ ${os_version} -lt 8 ]; then
         echo -e "错误: 请使用 Debian 8 或更高版本的系统！\n"
         :exit 1
     fi
